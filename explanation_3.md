6328a07a0ab87ac3ec4d2c8d5b2b2c4c26447b2c
Tonya Mork <hellofromtonya@knowthecode.io>
2019-06-10 13:45:27 -0500

    Added problem 3 boilerplate.

---
+++ explanation_3.md
@@ -0,0 +1,15 @@
+# Explanation for Problem 3: Huffman Coding
+
+This document provides an explanation for the design decisions and implementation.
+
+## Data Structures
+
+
+## Efficiencies
+
+
+## Design Considerations
+
+
+## Summary
+
---
+++ tests_3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import unittest
+from problem_3 import huffman_encoding, huffman_decoding
+
+
+class Test_HuffmanCoding(unittest.TestCase):
+
+    def test_should_(self):
+        """
+        Test huffman_encoding() should [some expected behavior] when [some edge case].
+        """
+        self.assertTrue(True)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
